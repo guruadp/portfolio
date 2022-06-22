c224cd860f4521c39a7050476090bd4b3ca2ca7f
guruadp <gurunandhanadp@gmail.com>
2022-06-22 11:31:36 +0530

    Initial commit

---
+++ src/app/api/experiencesEn.ts
@@ -0,0 +1,38 @@
+export const experiencesEn = [
+    {
+        title: "Software Development Engineer",
+        company: 'alfaTKG, Chennai, Tamilnadu, India',
+        period: "Nov 2020 - June 2022",
+        technologies: 'Python, Artificial Intelligence, C#, Angular',
+        missions: [
+            "Created an Artificial Intelligence model for forecasting the demand for a quarter for more than 2000 automobile parts with 10 years of order data with Long Short Term Memory (LSTM) algorithm using Tensorflow.",
+            "Developed an AI model for prediction of various costs such as manufacturing, processing, tool, and indirect.",
+            "Worked on image processing related projects such as creating a tool for re-orientation of scanned 2D drawing images and enhancing it using OpenCV-python and Image separation using GDPicture.NET.",
+            "Supported Enterprise Resource Planning team on data manipulation for 4 customers in Python and C\#.",
+            "Simulated industrial robots using RoboDK for applications such as pick and place and welding.",
+            "Deployed 5 Kiosk applications using Angular framework for 2 customers.",
+        ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Software Developer Intern",
+        company: 'alfaTKG, Chennai, Tamilnadu, India',
+        period: "June 2020 - Oct 2020",
+        technologies: 'Python, Pandas, Image Processing, OpenCV, C#',
+        missions: [
+            "",
+        ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Student Intern",
+        company: 'Hirano Co. Ltd, Asahi, Chiba Prefecture, Japan',
+        period: "June 2019 - July 2019",
+        technologies: 'Python, Pandas, Machine Learning, Sklearn',
+        missions: [
+            "Implemented a ML model using sklearn for predicting the bending parameters in sheet metal bending by collecting own dataset by designing in Solidworks and built a web app using streamlit"
+        ],
+        detailIsDisplayed: false,
+    },
+];
+
